b29d41092212d937478f41b91a0804db57b8b03b
Anaboliks <GrigorOgannisyan.12@gmail.com>
2023-10-30 17:57:26 +0300

    111

--- TFYA/KR_V_11/lexer.py
+++
@@ -1,9 +0,0 @@
-from rply import LexerGenerator
-
-class Lexer():
-    def __init__(self):
-        self.lexer = LexerGenerator()
-
-    def _add_tokens(self):
-        #Print
-        self.lexer.add('PRINT')
