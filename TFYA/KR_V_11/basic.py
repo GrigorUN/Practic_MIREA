ee1f4003fbf40449462056b73bb8412547574ccb
Anaboliks <GrigorOgannisyan.12@gmail.com>
2023-11-02 15:17:30 +0300

    лексер

--- TFYA/KR_V_11/basic.py
+++ TFYA/KR_V_11/basic.py
@@ -3,17 +3,43 @@
 DIGITS ='0123456789'
 #ERROR
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
 
     def as_string(self):
         result = f'{self.error_name} {self.details}'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Ошибка!', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end,'Ошибка!', details)
+
+# ПОЗИЦИЯ ОШИБКИ 
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+
+        return self
+    
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+    
 
 
 #ТОКЕНЫ
@@ -25,6 +51,10 @@ TT_MUL = 'MUL'
 TT_DIV= 'DEL'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
+# TT_PR = 'PROGRAM'
+# TT_VAR = 'VAR'
+# TT_BEGIN = 'BEGIN'
+# TT_END = 'END'
 
 class Token:
     def __init__(self, type_, value = None):
@@ -38,15 +68,16 @@ class Token:
 
 
 class Lexer:
-    def __init__(self, text): #инициализация текста
+    def __init__(self, fn, text): #инициализация текста
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
 
     def advance(self): #переход к след символу
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     #Создание токенов
     def make_tokens(self):
@@ -75,10 +106,23 @@ class Lexer:
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
+            # elif self.current_char == 'program':
+            #     tokens.append(Token(TT_PR))
+            #     self.advance()
+            # elif self.current_char == 'var':
+            #     tokens.append(Token(TT_VAR))
+            #     self.advance()
+            # elif self.current_char == 'begin':
+            #     tokens.append(Token(TT_BEGIN))
+            #     self.advance()
+            # elif self.current_char == 'end':
+            #     tokens.append(Token(TT_END))
+            #     self.advance()
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
+                return [], IllegalCharError(pos_start, self.pos,"'" + char + "'\n")
 
         return tokens, None
     
@@ -102,8 +146,8 @@ class Lexer:
         
 
 #Запуск
-def run(text):
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
 
     return tokens, error
