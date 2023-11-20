002a8af7531f646458cedbcc923986eb383b985f
GrigorUN <grigorogannisyan.12@gmail.com>
2023-11-20 18:32:24 +0300

    1

--- AVMS/Quartus/LAB_2_2/simulation/qsim/work/@l@a@b_2_2_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity LAB_2_2_vlg_check_tst is
-    port(
-        y               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end LAB_2_2_vlg_check_tst;
