e58925cc87a6c025a1cd44e70ba33f8bccf52cbf
GrigorUN <grigorogannisyan.12@gmail.com>
2023-11-20 16:55:17 +0300

    ukukyguk

---
+++ AVMS/Quartus/LAB_2_1/simulation/qsim/work/@l@a@b_2_1_vlg_check_tst/_primary.vhd
@@ -0,0 +1,8 @@
+library verilog;
+use verilog.vl_types.all;
+entity LAB_2_1_vlg_check_tst is
+    port(
+        LESS            : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end LAB_2_1_vlg_check_tst;
---
+++ AVMS/Quartus/LAB_2_2/simulation/qsim/work/@l@a@b_2_2/_primary.vhd
@@ -0,0 +1,13 @@
+library verilog;
+use verilog.vl_types.all;
+entity LAB_2_2 is
+    port(
+        a0              : in     vl_logic;
+        a1              : in     vl_logic;
+        a2              : in     vl_logic;
+        b0              : in     vl_logic;
+        b1              : in     vl_logic;
+        b2              : in     vl_logic;
+        y               : out    vl_logic
+    );
+end LAB_2_2;
