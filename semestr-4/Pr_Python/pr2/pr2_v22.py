44c11ec4c653ac1e30ac00ca0818c59cd851a7ed
GrigorUN <155751051+amazonooo@users.noreply.github.com>
2024-02-17 23:18:49 +0300

    esf

--- semestr-4/Pr_Python/pr2/pr2_v22.py
+++ semestr-4/Pr_Python/pr2/pr2_v22.py
@@ -4,9 +4,9 @@ import math
 def main(z):
     if z < 2:
         return (z ** 2 + 75 * z) ** 7
-    elif z >= 50:
-        return (68 ** (z ** 3 - 46 * z) ** 4 - (z - z ** 3) ** 5 - 79 * z ** 3)
-    else:
+    elif 2 <= z < 50:
         return ((z - 69 - z ** 3) ** 6 + z ** 2 + 36 * z ** 3)
-    
+    else:
+        return (68 ** (z ** 3 - 46 * z) ** 4 - (z - z ** 3) ** 5 - 79 * z ** 3)
+
 print(main(147))
