2feba405da259626d318bf92ddd0868c702a8753
amazonooo <155751051+amazonooo@users.noreply.github.com>
2024-02-17 23:55:13 +0300

    y

---
+++ semestr-4/Pr_Python/pr3/pr3_V22.py
@@ -0,0 +1,13 @@
+import math
+
+
+def main(m, n, a, p):
+    res1, res2 = 0, 0
+    for i in range(1, m + 1):
+        res1 += (math.atan(i) ** 4)
+    for i in range(1, a + 1):
+        for c in range(1, n + 1):
+            res2 += (70 * i ** 4 + 83 * c + (math.log2(p)) ** 6)
+    return (res1-res2)
+
+print(main(8, 3, 2, 0.36))
