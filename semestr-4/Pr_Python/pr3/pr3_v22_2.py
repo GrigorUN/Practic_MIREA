1e07ab00eb04e9193771b62575f8fc4f3c636bf0
GrigorUN <grigorogannisyan.12@gmail.com>
2024-02-28 10:48:27 +0300

    12

--- semestr-4/Pr_Python/pr3/pr3_v22_2.py
+++ semestr-4/Pr_Python/pr3/pr3_v22_2.py
@@ -1,11 +1,11 @@
-import math
+from math import atan, log2
 
 
 def main(m, n, a, p):
     res1, res2 = 0, 0
     for i in range(1, m + 1):
-        res1 += (math.atan(i) ** 4)
+        res1 += atan(i) ** 4
     for i in range(1, a + 1):
         for c in range(1, n + 1):
-            res2 += (70 * i ** 4 + 83 * c + (math.log2(p)) ** 6)
-    return (res1-res2)
+            res2 += 70 * i**4 + 83 * c + (log2(p)) ** 6
+    return res1 - res2
